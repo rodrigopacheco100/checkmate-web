8a70e0c673f190ae7addb94724b2e8e660759411
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2023-01-26 13:13:35 -0300

    feat: :sparkles: create home page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body>
+    <Html lang="pt-br">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
+      </Head>
+      <body className="bg-checkmate-gray-600">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
